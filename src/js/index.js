210b66caac4e7b80ec9e303ac67b0ad41c90f2f1
Illyuha1705 <75776668+Illyuha1705@users.noreply.github.com>
2021-06-09 02:30:10 +0300

    loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,21 @@
 "use strict";
 
-const questions = {
-    1: 'Как называют манекенщицу супер-класса?',
-    2: 'Кто вырос в джунглях среди диких зверей?',
-    3: 'Как называлась детская развлекательная программа, популярная в прошлые годы?',
-};
+const loader = document.querySelector('.loader')
+loader.style.opacity = 1;
 
-const answer1 = {
-    a: 'Топ-модель',
-    b: 'Тяп-модель',
-    c: 'Поп-модель',
-    d: 'Ляп-модель',
-}
-
-const answer2 = {
-    a: 'Колобок',
-    b: 'Маугли',
-    c: 'Бэтмен',
-    d: 'Бэтмен',
-}
-
-const answer3 = {
-    a: 'АБВГДейка',
-    b: 'ЁКЛМНейка',
-    c: 'ЁПРСТейка',
-    d: 'ЁЖЗИКейка',
-}
-
-function Welcome(name) {
-    alert(`Приветствую Вас, ${name}, в игре "Кто хочет стать миллионером?"` );
-}
-
-function CheckInput(answer) {
-    answer = answer.toLowerCase();
+setTimeout(() => {
+    const game = document.querySelector('.game');
     
-    if(answer == 'a') {
-        alert('Вверно! Поздравляем с правильным ответом!');
-    } else {
-        alert('Жаль! Но Вы проиграли(');
-    }   
-}
-
-function ShowRules() {}
-
-function StartGame() {
-    alert(questions[1], '');
-
-    let answer;
-    do {
-        answer = prompt(`A: ${answer1.a}, B: ${answer1.b}, C: ${answer1.c}, D: ${answer1.d}`, '0');
-    } while (answer);
-    
-    CheckInput(answer);
-}
+    game.style.display = 'block';
+    loader.style.display = 'none';
+}, 2000)
 
 
-Welcome('Kostya');
-StartGame();
+
+const interval = setInterval(() => {
+    loader.style.opacity -= 0.25;
+    console.log(loader.style.opacity);
+    if(loader.style.opacity <= 0) {
+        clearInterval(interval);
+    }
+}, 500);
